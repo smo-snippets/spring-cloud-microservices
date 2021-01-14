887ceee7e134f86b68014cb6daa10f7f877e89f7
Nils Smolinski <nils.smolinski@lyth.de>
2021-01-14 13:50:35 +0100

    init Currency Exchange + Currency Converter + Naming Server

---
+++ currency-exchange-service/src/test/java/de/smotastic/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.smotastic.microservices.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
